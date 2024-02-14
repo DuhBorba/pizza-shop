becc83d0a9bd42359de4d99b2a6978154911ee59
Eduardo Luis Borba <eduardoborba3@gmail.com>
2024-02-14 17:57:41 -0300

    ThemeToggle and Account menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import './global.css'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
 
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <Toaster richColors closeButton />
-      <RouterProvider router={router} />
+        <Toaster richColors closeButton />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
